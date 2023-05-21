7f7b3ca2d9f23494bc932e86cb3416b150c88cb6
Sean-Fuhrman <stfuhrma@ucsd.edu>
2023-05-21 14:20:56 -0700

    explore updates

--- sw.js
+++ sw.js
@@ -31,26 +31,8 @@ self.addEventListener('activate', function (event) {
 });
 
 // Intercept fetch requests and cache them
-self.addEventListener('fetch', function (event) {
-  // We added some known URLs to the cache above, but tracking down every
-  // subsequent network request URL and adding it manually would be very taxing.
-  // We will be adding all of the resources not specified in the intiial cache
-  // list to the cache as they come in.
-  /*******************************/
-  // This article from Google will help with this portion. Before asking ANY
-  // questions about this section, read this article.
-  // NOTE: In the article's code REPLACE fetch(event.request.url) with
-  //       fetch(event.request)
-  // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
-  /*******************************/
-  // B7. TODO - Respond to the event by opening the cache using the name we gave
-  //            above (CACHE_NAME)
-
- 
-
-
 self.addEventListener('fetch', (event) => {
-  // Check if this is a request for an image
+    console.log(event.request)
     event.respondWith(caches.open(CACHE_NAME).then((cache) => {
       // Go to the cache first
       return cache.match(event.request).then((cachedResponse) => {
@@ -66,8 +48,7 @@ self.addEventListener('fetch', (event) => {
 
           // Return the network response
           return fetchedResponse;
-        });
       });
-    }));
-  })
+    });
+  }));
 });
